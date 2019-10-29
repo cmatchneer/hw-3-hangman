7a27f23b7aea91d18d8822a312cf3ecf9ca30cef
cmatchneer <cmatchneer@yahoo.com>
2019-10-29 01:22:26 -0400

    got letters to show up when correcly guessed in right order as well still working on game logic for loop

--- assets/javacript/java.js
+++ assets/javacript/java.js
@@ -1,15 +1,24 @@
 var theList = ["the", "jumbo", "javascript", "html", "css", "gravity", "codeing", "bug", "debugging"];
 var numberOfGuesses = 5;
 var computerGuess = theList[Math.floor(Math.random() * theList.length)];
+var theLetters = [];
 console.log(computerGuess);
+for (var j = 0; j < computerGuess.length; j++) {
+    theLetters[j] = "_";
+}
+$("#theWord").append(theLetters);
+
 
 function theGame(guess) {
     var corretGuess = "you guessed the correct letter yayy!!"
     var wrongGuess = "you missed a letter uh-oh!!";
 
+
     for (var i = 0; i < computerGuess.length; i++) {
 
         if (guess === computerGuess.charAt(i)) {
+            theLetters[i] = guess;
+            $("#theWord").append(theLetters);
             $("#playerGuess").append(guess);
             // console.log(i)
 
@@ -17,6 +26,7 @@ function theGame(guess) {
 
 
         } else {
+            $("#playerGuess").append(guess);
             console.log(numberOfGuesses -= 1);
 
         }
