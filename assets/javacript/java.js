cfda2e1dcdb0c24ef948eac44b578cf059d48a40
cmatchneer <cmatchneer@yahoo.com>
2019-10-30 15:16:34 -0400

    the core game works need to figure out how to get the game to end

--- assets/javacript/java.js
+++ assets/javacript/java.js
@@ -1,42 +1,37 @@
 var theList = ["the", "jumbo", "javascript", "html", "css", "gravity", "codeing", "bug", "debugging"];
-var numberOfGuesses = 5;
+var numberOfGuesses = 15;
 var computerGuess = theList[Math.floor(Math.random() * theList.length)];
 var theLetters = [];
+var theWin = computerGuess.length;
 console.log(computerGuess);
+console.log(theWin);
 for (var j = 0; j < computerGuess.length; j++) {
-    theLetters[j] = "_";
+    theLetters[j] = " _ ";
 }
-$("#theWord").append(theLetters);
+$("#theWord").text(theLetters);
 
 
 function theGame(guess) {
-    var corretGuess = "you guessed the correct letter yayy!!"
-    var wrongGuess = "you missed a letter uh-oh!!";
-
-
+    numberOfGuesses -= 1;
+    $("#guessesLeft").text("Guesses Left: " + numberOfGuesses);
+    $("#playerGuess").append(guess);
+    $("#hitOrMiss").text("Uh oh " + guess + " isnt in the word keep going");
     for (var i = 0; i < computerGuess.length; i++) {
-
-        if (guess === computerGuess.charAt(i)) {
+        if (guess.toLowerCase() === computerGuess.charAt(i)) {
             theLetters[i] = guess;
+            theWin -= 1;
             $("#theWord").text(theLetters.join(""));
-            $("#playerGuess").append(guess);
-            // console.log(i)
-
-
-
-
-        } else {
-            numberOfGuesses -= 1;
-            $("#guessesLeft").text("Guesses Left: " + numberOfGuesses);
-            $("#playerGuess").append(guess);
-            console.log(numberOfGuesses);
-
+            $("#hitOrMiss").text("You got a letter only " + theWin + " to go");
 
         }
+        // if (theWin === 0) {
+        //     $("#endgame").text("YOU WIN YAYYYY!!!!! hit another letter to play again");
+        //     console.log(theWin);
+        //     return;
+        // }
     }
 
 }
-
 document.onkeypress = function(event) {
     var userGuess = event.key;
     theGame(userGuess);
