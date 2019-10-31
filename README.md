8df3cda718c2ca4fe1826f98cd4d2627ebb87357
cmatchneer <cmatchneer@yahoo.com>
2019-10-31 17:34:29 -0400

    reset and help button done and working

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # hw-3-hangman
+This is my attempt at a hang man game. The game has set number of words it picks from and uses Jquery and javascript to allow players to guess letters and see if they can firgure out the word before they run out of guesses. I used a fair share of google to create parts and look at how other people made games like this. This game also has reset button i called quit.
+https://nostarch.com/download/JS4K_ch7.pdf
+This source helped me the most it helped me wrap my head around how to create basic elements of the game that i thought would be very diffcult. I also looked at w3scools.com to help me understand basics of jQuery.
--- assets/javacript/java.js
+++ assets/javacript/java.js
@@ -1,10 +1,21 @@
 $(document).ready(function() {
     //base vars to build the game
-    var theList = ["the", "jumbo", "javascript", "html", "css", "gravity", "codeing", "bug", "debugging"];
+    var theList = ["the", "jumbo", "javascript", "html", "css", "gravity", "codeing", "bugs", "debugging"];
     var numberOfGuesses = 15;
     var computerGuess = theList[Math.floor(Math.random() * theList.length)];
     var theLetters = [];
     var theWin = computerGuess.length;
+    var tips = {
+        the: "You use this word everyday starts with t",
+        jumbo: "Some shrimps are called this",
+        javascript: "Used to make a web page dyanmic",
+        html: "Basics of makeing a website",
+        css: "Makes webpages look goooooooood",
+        gravity: "What keeps us from being able to fly",
+        codeing: "What people who create webpages and app and such do",
+        bugs: "The one thing coders and all homeowners hate to see ",
+        debugging: "What coders spend 90% of their lives doing"
+    };
     console.log(computerGuess);
 
     // making the array the same length as the guess and blank so it can be filled in later
@@ -17,30 +28,41 @@ $(document).ready(function() {
 
     //reset button
     $("#reset").on("click", function() {
-        // console.log(computerGuess);
-        $("#endGame").html("Looks like we have quitter over here" + "<br>" + "The word was " + computerGuess + "<br>" +
-            "Hit another letter to try again dont quite this time");
-        numberOfGuesses = 15;
-        computerGuess = theList[Math.floor(Math.random() * theList.length)];
-        theWin = computerGuess.length;
-        theLetters = []
-        $("#theWord").empty();
-        $("#playerGuess").empty();
-        $("#playerGuess").text("Your Guesses: ");
-        $("#hitOrMiss").empty();
-        $("#guessesLeft").text("Guesses Left: " + numberOfGuesses);
+            // console.log(computerGuess);
+            $("#endGame").html("Looks like we have quitter over here" + "<br>" + "The word was " + computerGuess + "<br>" +
+                "Hit another letter to try again dont quite this time");
+            numberOfGuesses = 15;
+            computerGuess = theList[Math.floor(Math.random() * theList.length)];
+            theWin = computerGuess.length;
+            theLetters = []
+            $("#theTip").empty();
+            $("#theWord").empty();
+            $("#playerGuess").empty();
+            $("#playerGuess").text("Your Guesses: ");
+            $("#hitOrMiss").empty();
+            $("#guessesLeft").text("Guesses Left: " + numberOfGuesses);
 
-        for (var r = 0; r < computerGuess.length; r++) {
-            theLetters[r] = " _ ";
-        }
-        $("#theWord").text(theLetters.join(""));
+            for (var r = 0; r < computerGuess.length; r++) {
+                theLetters[r] = " _ ";
+            }
+            $("#theWord").text(theLetters.join(""));
+        })
+        //tip button
+    $("#helpDesk").on("click", function() {
+        var tipsAccess = computerGuess;
+        $("#theTip").html("Welcome to the help desk here is your tip: " + tips[tipsAccess] + "<br>" +
+            "I hope this helps good luck");
+        $("#endGame").empty();
+
+
+
     })
 
     function theGame(guess) {
         //all the stuff that should happen hit or miss
         numberOfGuesses -= 1;
         $("#guessesLeft").text("Guesses Left: " + numberOfGuesses);
-        $("#playerGuess").append(guess);
+        $("#playerGuess").append(" " + guess + " ");
         $("#hitOrMiss").text("Uh oh " + guess + " isnt in the word keep going");
         $("#endGame").empty();
 
@@ -63,6 +85,7 @@ $(document).ready(function() {
             computerGuess = theList[Math.floor(Math.random() * theList.length)];
             theWin = computerGuess.length;
             theLetters = [];
+            $("#theTip").empty();
             $("#endGame").html("YOU ARE THE CHAMPION OF THE WORLD!!!!!" + "<br>" + "hit another letter to play again");
             $("#theWord").empty();
             $("#playerGuess").empty();
@@ -77,11 +100,12 @@ $(document).ready(function() {
         }
         //losing the game
         if (numberOfGuesses === 0) {
+            $("#endGame").html("LOOOOOOOSSSSSSEEEEEERRRRR!!!!!" + "<br>" + "The word you were tring to guess was " + computerGuess + "<br>" + "hit another letter to play again");
             numberOfGuesses = 15;
             computerGuess = theList[Math.floor(Math.random() * theList.length)];
             theWin = computerGuess.length;
             theLetters = [];
-            $("#endGame").html("LOOOOOOOSSSSSSEEEEEERRRRR!!!!!" + "<br>" + "hit another letter to play again");
+            $("#theTip").empty();
             $("#theWord").empty();
             $("#playerGuess").empty();
             $("#playerGuess").text("Your Guesses: ");
