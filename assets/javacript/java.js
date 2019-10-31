d9f3f3ca091e98df2e131c53a867bf4db8314e73
cmatchneer <cmatchneer@yahoo.com>
2019-10-31 14:37:41 -0400

    game fully functions tweaking it and make it look good

--- assets/javacript/java.js
+++ assets/javacript/java.js
@@ -4,20 +4,22 @@ var computerGuess = theList[Math.floor(Math.random() * theList.length)];
 var theLetters = [];
 var theWin = computerGuess.length;
 console.log(computerGuess);
-console.log(theWin);
+
 
 for (var j = 0; j < computerGuess.length; j++) {
     theLetters[j] = " _ ";
 }
-$("#theWord").append(theLetters);
+$("#theWord").text(theLetters);
 
 
 
 function theGame(guess) {
     numberOfGuesses -= 1;
+
     $("#guessesLeft").text("Guesses Left: " + numberOfGuesses);
     $("#playerGuess").append(guess);
     $("#hitOrMiss").text("Uh oh " + guess + " isnt in the word keep going");
+    $("#endGame").empty();
     for (var i = 0; i < computerGuess.length; i++) {
         if (guess.toLowerCase() === computerGuess.charAt(i)) {
             theLetters[i] = guess;
@@ -31,22 +33,37 @@ function theGame(guess) {
     }
     if (theWin === 0) {
         numberOfGuesses = 15;
+        computerGuess = theList[Math.floor(Math.random() * theList.length)];
+        theWin = computerGuess.length;
+        theLetters = [];
+        console.log(theWin);
+        console.log(computerGuess);
         $("#endGame").text("YOU ARE THE CHAMPION OF THE WORLD!!!!!");
         $("#theWord").empty();
         $("#playerGuess").empty();
         $("#hitOrMiss").empty();
         $("#guessesLeft").text("Guesses Left: " + numberOfGuesses);
-
-
+        for (var k = 0; k < computerGuess.length; k++) {
+            theLetters[k] = " _ ";
+        }
 
     }
     if (numberOfGuesses === 0) {
         numberOfGuesses = 15;
+        computerGuess = theList[Math.floor(Math.random() * theList.length)];
+        theWin = computerGuess.length;
+        theLetters = [];
         $("#endGame").text("LOOOOOOOSEEEEEERRRRR!!!!!");
         $("#theWord").empty();
         $("#playerGuess").empty();
         $("#hitOrMiss").empty();
         $("#guessesLeft").text("Guesses Left: " + numberOfGuesses);
+        console.log(theWin);
+        console.log(numberOfGuesses);
+        console.log(computerGuess);
+        for (var l = 0; l < computerGuess.length; l++) {
+            theLetters[l] = " _ ";
+        }
 
 
     }
